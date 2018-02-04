b97597ac1fc3d692e375beb7782541182aff1eae
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-02-04 13:29:31 +0100

    odczyt z pliku vehicle.csv

--- src/main/java/pl/sda/KomisApplication.java
+++ src/main/java/pl/sda/KomisApplication.java
@@ -2,7 +2,9 @@ package pl.sda;
 
 import pl.sda.csvUtils.CsvFile;
 import pl.sda.model.Person;
+import pl.sda.model.Vehicle;
 import pl.sda.readers.PeopleReader;
+import pl.sda.readers.VehicleReader;
 
 import java.io.IOException;
 import java.util.LinkedList;
@@ -14,6 +16,15 @@ public class KomisApplication {
         try {
             return new PeopleReader()
                     .read(new CsvFile("src/main/resource/people1.csv"));
+        } catch (IOException e) {
+            return new LinkedList<>();
+        }
+    }
+
+    public List<Vehicle> getVehiclesCollection() {
+        try {
+            return new VehicleReader()
+                    .read(new CsvFile("src/main/resource/vehicle.csv"));
         } catch (IOException e) {
             return new LinkedList<>();
         }
